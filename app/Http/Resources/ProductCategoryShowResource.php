5fddea79d0579b02749c714784880f0004d1148b
aavday <wrck7@yandex.ru>
2022-09-15 21:20:36 +0300

    Вывод брендов в categoryBySlug

--- app/Http/Resources/ProductCategoryShowResource.php
+++ app/Http/Resources/ProductCategoryShowResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\ProductBrand;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -17,13 +18,22 @@ class ProductCategoryShowResource extends JsonResource
      */
     public function toArray($request)
     {
+        $brandIds = [];
+
+        foreach ($this->products as $product) {
+            $brandIds[] = $product->id;
+        }
+
         return [
             'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
             'parent_category_id' => $this->parent_category_id,
             'child_categories' => ProductCategoryShowResource::collection($this->childCategories),
-            'products' => ProductIndexResource::collection($this->products)
+            'products' => ProductIndexResource::collection($this->products),
+            'brands' => ($brandIds)
+                ? ProductBrandIndexResource::collection(ProductBrand::query()->where('id', '=', $brandIds)->get())
+                : null
         ];
     }
 }
